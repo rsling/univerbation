e7f3b73e89fc78b99dc81cbf5d4627756652de20
Roland Schäfer <roland.schaefer@fu-berlin.de>
2022-03-26 20:48:34 +0100

    Comments from Elizabeth, rest. More fixes.

--- Corpus/RCorpus/Glm.R
+++ Corpus/RCorpus/Glm.R
@@ -48,3 +48,51 @@ corpus.glmm <- glmer(cbind(Joint, Separate)~Context+Relation+Link+(1|Compound),
 summary(corpus.glmm)
 corpus.fixefs <- fixef(corpus.glmm)
 nrow(concordance.glmm)
+
+
+
+
+
+
+corpus.fixefs <- fixef(corpus.glmm)
+corpus.confints <- confint(corpus.glmm)
+# corpus.confints <- cbind(corpus.fixefs, corpus.fixefs) # Fake CIs for quick recompiles.
+corpus.glmm.r2 <- r.squaredGLMM(corpus.glmm)
+
+
+
+
+format.ranef <- function(glmm, ranef) {
+  require(lme4)
+  .vcov   <- as.data.frame(VarCorr(glmm))
+  list(Name = ranef, Intercept = .vcov[which(.vcov$grp == ranef), "vcov"], sd = .vcov[which(.vcov$grp == ranef), "sdcor"])
+}
+
+# Build the table.
+corpus.ct <- cbind(corpus.fixefs, corpus.confints[2:8,])
+colnames(corpus.ct) <- c("Estimate", "CI low", "CI high")
+ranef.nv <- format.ranef(corpus.glmm, "Compound")
+corpus.r2.txt <- paste0("Nakagawa \\& Schielzeth's \\CM{R^2_m=", nice.float(corpus.glmm.r2[1,1]), "} and \\CM{R^2_c=", nice.float(corpus.glmm.r2[1,2]), "}")
+ranef.txt <- paste0("Random effect for N+V lemma: \\CM{Intercept=", nice.float(ranef.nv$Intercept), "}, \\CM{sd=", nice.float(ranef.nv$sd),
+                    "}")
+corpus.ctxt <- xtable(corpus.ct, digits = 3,
+                      caption = paste0("Coefficient table for the binomial GLMM modelling the corpus data with 95\\% profile likelihood ratio confidence intervals. The horizontal line separates first-level and second-level effects. Weighting was used to account for the bias in models on proportion data. ", ranef.txt, ". The intercepts model the fixed effects Relation~=~Argument. " , corpus.r2.txt),
+                      label = "tab:corpusglmm")
+
+# Experimental function to fix variable names.
+lme4.pretty <- function(s) {
+  s <- gsub("([a-z])([A-Z])", "\\1 = \\2", s)
+  s <- gsub("Object", "Argument", s)
+  gsub("Adjunct", "Oblique", s)
+}
+
+# Print the table.
+print(corpus.ctxt,
+      include.rownames=T,
+      floating = T,
+      table.placement = '!htbp',
+      booktabs = T,
+      scalebox = 1,
+      hline.after = c(-1,1,4,7),
+      sanitize.text.function = lme4.pretty
+)
